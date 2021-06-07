3f2c5a20afa139b6845a1c118cc7bcd9e1296c2a
Tiqur <trevorbrage02@gmail.com>
2021-06-07 04:00:10 -0700

    Setup nav links

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -2,7 +2,11 @@ import styles from './styles.module.scss';
 
 const NavBar = () => {
   return (
-    <p>NavBar</p>
+    <div className={styles.container}>
+      <p className={styles.hover_underline_animation}>Dashboard</p>
+      <p className={styles.hover_underline_animation}>Charts</p>
+      <p className={styles.hover_underline_animation}>Logs</p>
+    </div>
   )
 }
 
