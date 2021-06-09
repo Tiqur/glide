b24706c3b9c0b2989ab7fe6f7a515b0b64f4c30b
Tiqur <trevorbrage02@gmail.com>
2021-06-08 20:57:15 -0700

    Alert message

--- src/components/NotificationBell/NotificationBell.jsx
+++ src/components/NotificationBell/NotificationBell.jsx
@@ -4,6 +4,17 @@ import { ReactComponent as BellSvg } from '../../assets/notification.svg';
 import { GlobalContext } from '../GlobalContext.jsx';
 import { useState, useContext } from 'react';
 
+const Notification = (props) => {
+  return (
+    <div className={styles.notificationMessageContainer}>
+      <div className={styles.notificationMessageContent}>
+        <Text>4EMA: {props.token}</Text>
+      </div>
+      <div className={styles.notificationCloseButton}/>
+    </div>
+  )
+}
+
 const NotificationBell = (props) => {
   const { priceState, alertState } = useContext(GlobalContext);
   const [alertPopout, setAlertPopout] = useState(false);
@@ -20,7 +31,12 @@ const NotificationBell = (props) => {
       { alertPopout && 
         <>
           <div onClick={() => {setAlertPopout(false)}}className={styles.notificationPopoutOverlay}/>
-          <div className={styles.notificationPopout}/>
+          <div className={styles.notificationPopout}>
+            <Notification />
+            <Notification />
+            <Notification />
+            <Notification />
+          </div>
         </>
       }
       <div onClick={handleClick} className={styles.notificationContainer}>
