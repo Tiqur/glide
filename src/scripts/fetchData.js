d5a5c730bd9871b9f068236ccca940f798680a1f
Tiqur <trevorbrage02@gmail.com>
2021-06-09 17:20:07 -0700

    DownloadHistory method

--- src/scripts/fetchData.js
+++ src/scripts/fetchData.js
@@ -26,28 +26,52 @@ const intervalSecMap  = {
 // Class specific to each token
 // Holds Interval instances for each time interval
 class Token {
-  constructor() {
+  constructor(token, precision, ema_ranges, time_ranges) {
     this.token = token;
     this.precision = precision;
     this.ema_ranges = ema_ranges;
     this.time_ranges = time_ranges;
-    this.time_interval_instances = []''
+    this.time_interval_instances = [];
+  }
+
+  downloadHistory() {
+    // Download data for each time range and moving average range
+    time_ranges.forEach(time_range) {
+      // Initialize interval history
+      ih = TimeInterval(time_range);
+      
+      // Emas rely on previous emas.  Calculate extra for more precision.
+      download_range = Math.max(this.ema_ranges) + this.precision;
+
+      // Convert binance kline to seconds
+      interval_sec = intervalSecMap[time_range] * download_range;
+      
+      // Fetch historical data
+      const res = axios.get(`https://api.binance.com/api/v3/klines?symbol=${symbol}&interval=${interval}&startTime=${range}`);
+      const historical_data = res.json();
+
+      // Optimize downloads by only downloading the necessary data per token
+      this.ema_ranges.forEach(ma_range => {
+
+        // Initialize moving average interval instance
+        mai = MovingAverageInterval(ma_range);
+
+        // new data range
+        new_range = historical_data.slice(-(ma_range + self.precision));
+
+        // Organize data
+        new_range.forEach(data => {
+          mai.ohlcv.append(Ohlvc(data))
+        })
+
+        // Append moving average instance to TimeInterval
+        ih.moving_average_instances.append(mai);
+      })
+
+      this.time_interval_instances.append(ih);
+
+    }
   }
 }
 
 
-const fetchData = async (props) => {
-  const symbols = props.symbol;
-  const intervals = props.interval;
-  const rangeInDays = props.rangeInDays
-  
-  // Interval ( in seconds ) * amount of days
-  const range = rangeInDays * intervalSecMap[interval];
-
-  // Download data
-  const res = await axios.get(`https://api.binance.com/api/v3/klines?symbol=${symbol}&interval=${interval}&startTime=${range}`)
-
-  // Convert to json
-  const data = await res.json()
-  console.log(data)
-}
