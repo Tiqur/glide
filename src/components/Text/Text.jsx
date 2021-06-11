36e01a0a93ee6fd638b13b4321fdb56c45c37623
Tiqur <trevorbrage02@gmail.com>
2021-06-11 05:25:41 -0700

    Added inline property to Text component

--- src/components/Text/Text.jsx
+++ src/components/Text/Text.jsx
@@ -4,6 +4,7 @@ import styles from './styles.module.scss';
 const Text = (props) => {
   return (
     <p style={{
+      display: props.inline ? 'inline' : 'block',
       color: props.color ? props.color : '#B2B5BE',
       fontWeight: props.weight ? props.weight : 'initial',
       fontSize: (props.size ? props.size : 'initial') + 'ch',
