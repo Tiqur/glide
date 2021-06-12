2544810a1ca02bba5fb81788094b774f9fd613d3
Tiqur <trevorbrage02@gmail.com>
2021-06-12 00:47:26 -0700

    Only append if fully downloaded

--- src/components/OpenWebsockets.jsx
+++ src/components/OpenWebsockets.jsx
@@ -3,7 +3,8 @@ import { GlobalContext } from '../components/GlobalContext.jsx';
 import { useContext, useEffect } from 'react';
 
 const OpenWebsockets = () => {
-  const { logState, tokenDataState } = useContext(GlobalContext);
+  const { logState, statusState, tokenDataState } = useContext(GlobalContext);
+  const [status, setStatus] = statusState;
   const [logs, setLogs] = logState;
   const [tokenData, setTokenData] = tokenDataState;
 
@@ -27,26 +28,29 @@ const OpenWebsockets = () => {
       // Update current price in token data state
       if (token in tokenData) tokenData[token]['current_price'] = current_price;
 
-      // Merge data
+      // Merge data if fully downloaded
       Object.keys(tokenData).forEach(token => {
         Object.keys(tokenData[token]).forEach(time_interval => {
           // Make sure 'time_interval' isn't 'current_price'
           if (typeof tokenData[token][time_interval] === 'object' && tokenData[token][time_interval] && tokenData[token][time_interval]['ohlvc'].length > 0) {
             const ohlvc_arr = tokenData[token][time_interval]['ohlvc'];
-            const last_ohlvc = ohlvc_arr[ohlvc_arr.length-1];
-            
-            // Calculate the time between the current time, and the previous candle's end time.
-            // This will show us if we should update the current candle's close_price, or create a new candle
-            const time_between = time - last_ohlvc.end_time;
 
-            // If on same candle as last downloaded
-            if (time_between < 0) {
-              // Update price
-              last_ohlvc.close = current_price;
-            } else {
-              console.log("Append new")
+            // Make sure fully downloaded
+            if (ohlvc_arr.length === 1000 + 55) {
+              const last_ohlvc = ohlvc_arr[ohlvc_arr.length-1];
+              
+              // Calculate the time between the current time, and the previous candle's end time.
+              // This will show us if we should update the current candle's close_price, or create a new candle
+              const time_between = time - last_ohlvc.end_time;
+
+              // If on same candle as last downloaded
+              if (time_between < 0) {
+                // Update price
+                last_ohlvc.close = current_price;
+              } else {
+                console.log("Append")
+              }
             }
-
           }
         })
       })
